564f6ee1100377960fd6bff2630f62ad9fe48db6
Deepak Reddy <deepakreddy@Deepaks-MacBook-Pro.local>
2024-03-19 18:52:17 -0700

    read and write from s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,26 @@
 import json
-import pandas
+import pandas as pd
+import boto3
+
+s3_client = boto3.client('s3')
+sns_client = boto3.client('sns')
+sns_arn = 'arn:aws:sns:us-east-1:851725469799:s3-arrival-notification'
+
+output_bucket_name = 'doordash-target-zone-deepak'
 
 def lambda_handler(event, context):
+    
+    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+    s3_file_key = event["Records"][0]["s3"]["object"]["key"]
+    print(bucket_name)
+    print(s3_file_key)
+    resp = s3_client.get_object(Bucket=bucket_name, Key=s3_file_key)
+    print(resp['Body'])
+    data = pd.read_json(resp['Body'], lines=True)
+    filtered_data = data[data['status'] == 'delivered']
+    processed_json = filtered_data.to_json(orient='records', lines=True)
+    s3_client.put_object(Bucket=output_bucket_name, Key=s3_file_key, Body=processed_json)
+    print("processed_json = ", processed_json)
     print("events --> ", event)
     print("hello")
     
